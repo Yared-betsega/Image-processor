122e4ff13bfee1c41cf1e15f80f35d55e6916e13
Yared Tsegaye <yaredtsegaye120@gmail.com>
2024-04-11 16:35:19 +0300

    Add files

---
+++ main.py
@@ -0,0 +1,87 @@
+import requests
+import os
+from fastapi import FastAPI, UploadFile, File, Form
+from typing import List
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+from utils import convert_video_to_images, encode_frame, encode_image
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/process")
+async def process_files(text: str = Form(...), file: UploadFile = File(...)):
+    if file.content_type.startswith('video'):
+        video_path = f"./videos/{file.filename}"
+        await file.seek(0)
+
+        with open(video_path, "wb") as video_file:
+            video_file.write(file.file.read())
+        image_files = convert_video_to_images(video_path, 100)
+
+    elif file.content_type.startswith('image'):
+        image_files = [f"./images/{file.filename}"]
+        await file.seek(0)
+
+        with open(image_files[0], "wb") as image_file:
+            image_file.write(file.file.read())
+    else:
+        return {"error": "Invalid file type. Only images and videos are supported."}
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAI_API_KEY}"
+    }
+
+    content = []
+    text_question = {
+        "type": "text",
+        "text": text
+    }
+
+    content.append(text_question)
+
+    for image_file in image_files:
+        if file.content_type.startswith('video'):
+            base64_image = encode_frame(image_file)
+        else:
+            base64_image = encode_image(image_file)
+
+        content.append({
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
+            }
+        })
+
+        if len(content) > 10:
+            break
+    
+
+    payload = {
+        "model": "gpt-4-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": content
+            }
+        ],
+        "max_tokens": 300
+    }
+
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    return response.json()
---
+++ utils.py
@@ -0,0 +1,80 @@
+import base64
+import cv2
+import os
+import shutil
+
+def convert_video_to_images(video_path, frame_interval):
+    # Open the video file
+    video = cv2.VideoCapture(video_path)
+    
+    # Initialize frame count
+    frame_count = 0
+    
+    frames = []
+    
+    # Read the first frame
+    success, frame = video.read()
+    # Loop through the video frames
+    while success:
+        # Save the frame as an image if it's a necessary frame
+        if frame_count % frame_interval == 0:
+            frames.append(frame)
+        
+        # Read the next frame
+        success, frame = video.read()
+        
+        # Increment frame count
+        frame_count += 1
+    
+    # Release the video object
+    video.release()
+    
+    return frames
+
+def convert_video_to_images_and_save(video_path, frame_interval):
+    output_folder = "./images/"
+    # Open the video file
+    video = cv2.VideoCapture(video_path)
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
+    
+    # Clear the output folder
+    shutil.rmtree(output_folder)
+    os.makedirs(output_folder)
+    
+    # Initialize frame count
+    frame_count = 0
+    
+    # Read the first frame
+    success, frame = video.read()
+    # Loop through the video frames
+    while success:
+        # Save the frame as an image if it's a necessary frame
+        if frame_count % frame_interval == 0:
+            image_path = os.path.join(output_folder, f"frame_{frame_count}.jpg")
+            cv2.imwrite(image_path, frame)
+        
+        # Read the next frame
+        success, frame = video.read()
+        
+        # Increment frame count
+        frame_count += 1
+    
+    # Release the video object
+    video.release()
+    
+    return output_folder
+    
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def encode_frame(frame):
+    _, encoded_frame = cv2.imencode('.jpg', frame)
+    return base64.b64encode(encoded_frame).decode('utf-8')
+
+# Example usage
+# video_path = "./video_storage/Why do we dream  Amy Adkins_480p.mp4"
+# output_folder = "./converted_images_storage"
+# frame_interval = 100  # Extract every 10th frame
+# convert_video_to_images(video_path, frame_interval)
